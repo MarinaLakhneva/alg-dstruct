620b09cc103abbcad832a38b7624b2bf22706466
MarinaLakhneva <lahneva.myu@edu.spbstu.ru>
2021-10-02 00:50:55 +0300

    Изм. условия

--- 4-LakhnevaMarina-A18/src/lab_1.h
+++ 4-LakhnevaMarina-A18/src/lab_1.h
@@ -4,24 +4,24 @@
 #include <string.h>
 
 typedef struct list {
-    const char* value;
+    char* value;
     struct list* next;
 } list;
 
-list* create(const char* word) {
+list* create(char* word) {
     list* tmp = (list*)malloc(sizeof(list));
     if (tmp == NULL) {
-        +printf("Требуемая память не выделена!");
+        +printf("No memory allocated!"); 
     }
             tmp->value = word;
             tmp->next = NULL;
             return(tmp);
 }
 
-list* append(const char* word, list* head) {
+list* append(char* word, list* head) {
     list* tmp = (list*)malloc(sizeof(list));
     if (tmp == NULL) {
-        +printf("Требуемая память не выделена!");
+        +printf("No memory allocated!");
     }
             tmp->value = word;
             tmp->next = head;
@@ -41,11 +41,11 @@ void sort(list* fin) {
     list* finish = fin;
 
     while (fin->next != NULL) {
-        const char* value1 = fin->value;
-        const char* value2 = fin->next->value;
+        char* value1 = fin->value;
+        char* value2 = fin->next->value;
 
         if (strcmp(value2, value1) < 0) {
-            const char* temp = fin->value;
+            char* temp = fin->value;
             fin->value = fin->next->value;
             fin->next->value = temp;
             swap_counter = swap_counter + 1;
