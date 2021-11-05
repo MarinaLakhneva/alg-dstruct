e5e43525dbe6539802df92c8f0c5757b5a1f2ef5
MarinaLakhneva <lahneva.myu@edu.spbstu.ru>
2021-11-05 23:30:53 +0300

    96/100 (%) - внесла пару проверок в memalloc

--- 4-LakhnevaMarina-B11/src/Prg.cpp
+++ 4-LakhnevaMarina-B11/src/Prg.cpp
@@ -17,56 +17,47 @@ extern "C" {
     };
 
     Node* first_node = NULL;
+    int mem_size = 0;
 
     // Инициализировать систему памяти с помощью блока памяти pMemory.
     int meminit(void* pMemory, int size) {
-
         if (first_node != NULL)
             return -1;
-
         if (size <= sizeof(Node))
             return -1;
-
         if (pMemory == NULL)
             return -1;
 
-
         first_node = (Node*)pMemory;
-
         first_node->next_node = NULL;
         first_node->prev_node = NULL;
         first_node->is_free = true;
         first_node->size = size - sizeof(Node);
-
+        mem_size = size;
         return 0;
-
     }
 
     // Выделяем блок памяти размером 'size'.
     // Возвращает указатель на блок памяти, если он успешен, иначе 0
 
-    Node* get_first_fit(int size)
-    {
+    Node* get_first_fit(int size) {
         Node* current_node = first_node;
-        while (current_node != NULL)
-        {
-            if (current_node->size >= size)
+        while (current_node) {
+            if (current_node->size >= size && current_node->is_free)
                 return current_node;
-
             current_node = current_node->next_node;
         }
         return NULL;
     }
 
     void* memalloc(int size) {
-
+        if (first_node == NULL)
+            return NULL;
         Node* current_node = get_first_fit(size);
-
-
         if (current_node == NULL)
             return(NULL);
 
-        if (current_node->size - size >= sizeof(Node)) {
+        if (current_node->size - size > sizeof(Node)) {
             Node* new_node = (Node*)((char*)current_node + sizeof(Node) + size);
             new_node->size = current_node->size - size - sizeof(Node);
             new_node->next_node = current_node->next_node;
@@ -76,82 +67,77 @@ extern "C" {
             }
             new_node->prev_node = current_node;
             current_node->next_node = new_node;
-            current_node->size = size;
+            new_node->is_free = 1;
         }
+        current_node->is_free = 0;
+        current_node->size = size;
         return((char*)current_node + sizeof(Node));
     }
 
+
     // Освободить память, ранее выделенную memalloc
-    void  memfree(void* p)
-    {
+    void  memfree(void* p) {
         Node* current_node = (Node*)((char*)p - sizeof(Node));
-
         Node* node_for_merge;
-
         if (current_node == NULL)
             return;
 
-        if (current_node->is_free == 1)
+        if (current_node->is_free != 0)
             return;
-
         current_node->is_free = true;
-
-        if (current_node->next_node != NULL)
-        {
-            if (current_node->next_node->is_free == 1)
-            {
+        if (current_node->next_node != NULL) {
+            if (current_node->next_node->is_free == 1) {
                 node_for_merge = current_node;
-
                 node_for_merge->size = node_for_merge->size + sizeof(Node) + node_for_merge->next_node->size;
-
-                if (node_for_merge->next_node->next_node != NULL)
-                {
+                if (node_for_merge->next_node->next_node != NULL) {
                     node_for_merge->next_node->next_node->prev_node = node_for_merge;
-
                     node_for_merge->next_node = node_for_merge->next_node->next_node;
                 }
-                else
+                else {
                     node_for_merge->next_node = NULL;
-
+                }
             }
         }
 
-        if (current_node->prev_node != NULL)
-        {
-            if (current_node->prev_node->is_free == 1)
-            {
+        if (current_node->prev_node != NULL) {
+            if (current_node->prev_node->is_free == 1) {
                 node_for_merge = current_node->prev_node;
-
-                node_for_merge->size = node_for_merge->size + sizeof(Node) + node_for_merge->next_node->size;
-
-                if (node_for_merge->next_node->next_node != NULL)
-                {
+                if (node_for_merge->next_node != NULL) {
+                    node_for_merge->size = node_for_merge->size + sizeof(Node) + node_for_merge->next_node->size;
+                }
+                if (node_for_merge->next_node->next_node != NULL) {
                     node_for_merge->next_node->next_node->prev_node = node_for_merge;
-
                     node_for_merge->next_node = node_for_merge->next_node->next_node;
                 }
-                else
+                else {
                     node_for_merge->next_node = NULL;
+                }
             }
         }
-
-
     }
 
     // Здесь вы можете реализовать проверку на утечку памяти
     void  memdone() {
         Node* current_node = first_node;
+        int general_size = 0;
         while (current_node != NULL) {
-            if (current_node->is_free == false) {
-                printf("memory leak at address %p, size %d\n", (char*)current_node + sizeof(Node), current_node->size);
-            }
+            //            if (current_node->is_free == false) {
+            //                printf("memory leak at address %p, size %d\n", (char*)current_node + sizeof(Node), current_node->size);
+            //            }
+            general_size += current_node->size + sizeof(Node);
             current_node = current_node->next_node;
         }
+        if (general_size != mem_size) {
+            fprintf(stderr, "Memory leak");
+        }
+        first_node = NULL;
+        mem_size = 0;
     }
 
     int  memgetminimumsize() {
         return(sizeof(Node));
     }
+
     int  memgetblocksize() {
         return(sizeof(Node));
     }
